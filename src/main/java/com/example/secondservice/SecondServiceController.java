082b5453629aea9e1b267c7451bb03d5e9cf95de
sesam-me <lsi6601@gmail.com>
2024-01-13 03:36:05 +0900

    Feat: Load Balancer

--- src/main/java/com/example/secondservice/SecondServiceController.java
+++ src/main/java/com/example/secondservice/SecondServiceController.java
@@ -1,6 +1,8 @@
 package com.example.secondservice;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/second-service")
 @Slf4j
 public class SecondServiceController {
+    Environment env;
+
+    public SecondServiceController(Environment env) {
+        this.env = env;
+    }
     @GetMapping("/welcome")
     public String welcome() {
         return "welcome to the second service";
@@ -22,7 +29,10 @@ public class SecondServiceController {
     }
 
     @GetMapping("/check")
-    public String check() {
-        return "Hi, there. This is a message from Second Service.";
+    public String check(HttpServletRequest request) {
+        log.info("Server port={}", request.getServerPort()); // 방법1. HttpServletRequest
+        // 방법2. 환경변수에 할당된 정보값을 LocalServerPort로 가져오기
+        return String.format("Hi, there. This is a message from First Service on PORT %s.", env.getProperty("local.server.port"));
     }
+
 }
